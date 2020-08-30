9ba7be6b97831844dc5b21f5a0e5d10b627e7d35
Ademilson Almeida <ademilsonalmeida@aalmeida-nb.localdomain>
2020-08-29 21:42:22 -0300

    Resolução do desafio

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,17 @@
-// import AppError from '../errors/AppError';
+import { getRepository } from 'typeorm';
+import AppError from '../errors/AppError';
+
+import Transaction from '../models/Transaction';
+
+interface Request {
+  id: string;
+}
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
+  public async execute({ id }: Request): Promise<void> {
     // TODO
+    const transactionsRepository = getRepository(Transaction);
+    await transactionsRepository.delete(id);
   }
 }
 
